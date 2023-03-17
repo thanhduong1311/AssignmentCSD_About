402f46d6781795cf36660740e8848edec83eb21b
thanhduong1311 <thanhduongjnguyen@gmail.com>
2023-03-17 09:48:07 +0700

    javascript a litte

---
+++ main.js
@@ -0,0 +1,40 @@
+ var a = document.getElementsByClassName("cards-btn");
+var b = document.getElementsByClassName("card-contact");
+ function change1() {
+    var a = document.getElementById("card1")
+    if(a.classList.contains("unactive")) {
+        a.classList.remove("unactive")
+    } else {
+        a.classList.add("unactive")
+    }
+ }
+
+ function change2() {
+    var a = document.getElementById("card2")
+    if(a.classList.contains("unactive")) {
+        a.classList.remove("unactive")
+    } else {
+        a.classList.add("unactive")
+    }
+ }
+
+ function change3() {
+    var a = document.getElementById("card3")
+    if(a.classList.contains("unactive")) {
+        a.classList.remove("unactive")
+    } else {
+        a.classList.add("unactive")
+    }
+ }
+
+ function change4() {
+    var a = document.getElementById("card4")
+    if(a.classList.contains("unactive")) {
+        a.classList.remove("unactive")
+    } else {
+        a.classList.add("unactive")
+    }
+ }
+
+
+
